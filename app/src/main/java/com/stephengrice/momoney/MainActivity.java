184270628f1021557ed9e1340ace2c2e6e48c6fe
Stephen Grice <stephengrice@live.com>
2017-08-05 15:37:34 -0400

    Add basic dashboard, begin to convert to fragment

--- app/src/main/java/com/stephengrice/momoney/MainActivity.java
+++ app/src/main/java/com/stephengrice/momoney/MainActivity.java
@@ -1,5 +1,6 @@
 package com.stephengrice.momoney;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,17 +81,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        Intent intent;
 
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.nav_dash) {
+            intent = new Intent();
+        } else if (id == R.id.nav_budget) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_transactions) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_goals) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_add) {
 
         }
 
