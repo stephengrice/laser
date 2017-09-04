94e513a5f276f41f99d84c10793ac9649ef89069
Stephen Grice <stephengrice@live.com>
2017-09-04 18:34:19 -0400

    Add notifications

--- app/src/main/java/com/stephengrice/laser/AlarmReceiver.java
+++ app/src/main/java/com/stephengrice/laser/AlarmReceiver.java
@@ -2,20 +2,29 @@ package com.stephengrice.laser;
 
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.stephengrice.laser.db.DbContract;
 import com.stephengrice.laser.db.DbHelper;
+import com.stephengrice.laser.fragment.TransactionsFragment;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
@@ -55,7 +64,8 @@ public class AlarmReceiver extends BroadcastReceiver {
         // Update ScheduledTransaction object in DB
         DbHelper.updateScheduledTransaction(context, scheduledTransaction);
 
-        Toast.makeText(context, "Transaction added: " + scheduledTransaction.description + "; Next alarm: " + MainActivity.formatDate(context, scheduledTransaction.date), Toast.LENGTH_LONG).show();
+        sendNotification(context, scheduledTransaction);
+//        Toast.makeText(context, "Transaction added: " + scheduledTransaction.description + "; Next alarm: " + MainActivity.formatDate(context, scheduledTransaction.date), Toast.LENGTH_LONG).show();
     }
 
     public static void setAlarm(Context context, DbContract.ScheduledTransaction scheduledTransaction){
@@ -116,5 +126,44 @@ public class AlarmReceiver extends BroadcastReceiver {
                 break;
         }
         return calendar.getTimeInMillis();
+    }
+
+    public static void sendNotification(Context context, DbContract.ScheduledTransaction scheduledTransaction) {
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(context);
+        if (prefs.getBoolean(MainActivity.OPTION_ALLOW_NOTIFICATIONS, true)) {
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(context)
+                            .setSmallIcon(R.drawable.notification_laser_logo)
+                            .setContentTitle("Transaction added")
+                            .setContentText(MainActivity.formatCurrency(Math.abs(scheduledTransaction.amount)) + " has been "
+                                + (scheduledTransaction.amount >= 0 ? "added to" : "removed from")
+                                + " your Laser!")
+                            .setDefaults(Notification.DEFAULT_ALL)
+                            .setPriority(Notification.PRIORITY_HIGH)
+                            .setColor(ContextCompat.getColor(context, R.color.colorPrimary));
+
+            Intent resultIntent = new Intent(context, MainActivity.class);
+            resultIntent.putExtra(MainActivity.ARG_START_FRAGMENT, TransactionsFragment.class.getCanonicalName());
+            // Because clicking the notification opens a new ("special") activity, there's
+            // no need to create an artificial back stack.
+            PendingIntent resultPendingIntent =
+                    PendingIntent.getActivity(
+                            context,
+                            0,
+                            resultIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+
+            // Sets an ID for the notification
+            int mNotificationId = 1;
+            // Gets an instance of the NotificationManager service
+            NotificationManager mNotifyMgr =
+                    (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+            // Builds the notification and issues it.
+            mNotifyMgr.notify(mNotificationId, mBuilder.build());
+        }
     }
 }
